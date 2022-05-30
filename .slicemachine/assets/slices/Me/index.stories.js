b8e001f25a724f64e256482f9709e70278102838
Alice Sales <75091620+AliceSales@users.noreply.github.com>
2022-05-30 13:44:11 -0300

    Delete .slicemachine directory

--- .slicemachine/assets/slices/Me/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Me';
-
-export default {
-  title: 'slices/Me'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"me","items":[],"primary":{"title":[{"type":"heading1","text":"Enhance compelling synergies","spans":[]}],"description":[{"type":"paragraph","text":"Pariatur qui aute magna. Esse velit et nisi deserunt mollit ex do commodo reprehenderit velit reprehenderit nisi ex ut exercitation. Reprehenderit sit aliquip et elit anim culpa sunt duis.","spans":[]}]},"id":"_Default"}} />
-_Default.storyName = 'Default'
